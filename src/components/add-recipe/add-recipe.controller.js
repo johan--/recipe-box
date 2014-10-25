266d3f99a2a638f08bfebbbaf5c9a58064fe7919
Alexandria A. Johnson <alexandria.abdallah@gmail.com>
2014-10-25 18:18:40 -0500

    S3 upload and Firebase push now working together! :)

--- src/components/add-recipe/add-recipe.controller.js
+++ src/components/add-recipe/add-recipe.controller.js
@@ -10,11 +10,10 @@ recipeBox.controller('addRecipeCtrl', ['$scope', '$rootScope','$firebase', '$fir
 $rootScope.uid = localStorage.getItem('uid');
 
 var userFBURL = 'https://glowing-inferno-7484.firebaseIO.com/profiles/' + $rootScope.uid + '/recipes/'; 
-debugger;
+
 var ref = new Firebase(userFBURL);
 var sync = $firebase(ref);
 $scope.fb_deets = sync.$asArray();
-
 
 
 $scope.addRecipe = function(submission) {
@@ -22,6 +21,53 @@ $scope.addRecipe = function(submission) {
 }
         
 
+////S3 stuff/////
+
+$scope.creds = {
+  
+};
+
+$scope.onFileSelect = function($files) {
+  $scope.file = $files[0];
+};
+
+$scope.upload = function() {
+  // Configure The S3 Object
+  var bucket = new AWS.S3({
+    region: 'us-west-2',
+    credentials: new AWS.Credentials($scope.creds.access_key, $scope.creds.secret_key)
+  });
+
+  if($scope.file) {
+    var params = {
+      Bucket: $scope.creds.bucket,
+      Key: $scope.file.name,
+      ContentType: $scope.file.type,
+      Body: $scope.file,
+      ServerSideEncryption: 'AES256' };
+
+      bucket.putObject(params, function(err, data) {
+        if(err) {
+        // There Was An Error With Your S3 Config
+        alert(err.message);
+        return false;
+      }
+      else {
+        console.log(data);
+       
+        console.log('Upload Done');
+      }
+    })
+      .on('httpUploadProgress',function(progress) {
+          // Log Progress Information
+          console.log(Math.round(progress.loaded / progress.total * 100) + '% done');
+        });
+    }
+    else {
+    // No File Selected
+    alert('No File Selected');
+  }
+}
 
 }]);
 
